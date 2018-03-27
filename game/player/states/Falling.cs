6b665d85bd930f0dfb99c7d6c5d8e64126a12de7
Will Reynolds <arthurknightwood@gmail.com>
2018-03-26 21:10:27 -0500

    New: Player wall jump (fixes #74)

--- game/player/states/Falling.cs
+++ game/player/states/Falling.cs
@@ -4,21 +4,31 @@ using Godot;
 public class Falling : State<Player> {
     private bool heldJumpWhileEntering = false;
 
-    public override void OnEnter(float delta, Player owner) {
-        owner.PlayAnimation(Player.Animation.Falling);
-        this.heldJumpWhileEntering = owner.IsActionPressed("key_jump");
+    public override void OnEnter(float delta, Player player) {
+        player.PlayAnimation(Player.Animation.Falling);
+        this.heldJumpWhileEntering = player.IsActionPressed("key_jump");
+        player.ResetWallJump();
     }
 
     public override void OnExit(float delta, Player owner) {
     }
 
     public override State<Player> Update(float delta, Player player, float timeInState) {
+        int dir = player.GetInputDirection();
         player.DetectDirectionChange();
         if (player.IsOnFloor()) {
             return player.stateIdle;
         }
+        player.UpdateWallJump(delta);
         player.AirControl(delta);
         player.ApplyGravity(delta);
+
+        // Wall jump?
+        if(player.CanWallJump() && Input.IsActionJustPressed("key_jump")) {
+            player.ResetWallJump();
+            player.velocity = player.WallJump(dir);
+        }
+
         player.MoveX(player.velocity.x * delta, () => {
             player.velocity.x = 0;
         });
--- game/player/states/Jumping.cs
+++ game/player/states/Jumping.cs
@@ -5,6 +5,7 @@ public class Jumping : State<Player> {
     public override void OnEnter(float delta, Player player) {
         player.velocity = new Vector2(player.velocity.x, -Mathf.Sqrt(2 * player.gravity * player.jumpHeight));
         player.PlayAnimation(Player.Animation.Jumping);
+        player.ResetWallJump();
     }
 
     public override void OnExit(float delta, Player owner) {
@@ -22,8 +23,17 @@ public class Jumping : State<Player> {
         if(player.IsActionJustReleased("key_jump")) {
             player.velocity.y *= 0.5f;
         }
+        player.UpdateWallJump(delta);
         player.AirControl(delta);
         player.ApplyGravity(delta);
+
+        if(player.CanWallJump() && Input.IsActionJustPressed("key_jump")) {
+            GD.Print("JUMP!");
+            player.ResetWallJump();
+            GD.Print(player.wallJumpGraceTime);
+            player.velocity = player.WallJump(dir);
+        }
+
         player.MoveX(player.velocity.x * delta, () => {
             player.velocity.x = 0;
         });
